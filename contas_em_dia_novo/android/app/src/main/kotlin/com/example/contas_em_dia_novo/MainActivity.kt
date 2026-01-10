204dc36fd11252ac8b5fc0dbe2dc9ff825f15305
LeticiaRodrigues03 <leticia_rodriguesdesousa@hotmail.com>
2026-01-09 21:30:07 -0300

    Aplicativo contas em dia - versão inicial

---
+++ contas_em_dia_novo/android/app/src/main/kotlin/com/example/contas_em_dia_novo/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.contas_em_dia_novo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ contas_em_dia_novo/plugins/flutter_native_timezone/android/src/main/kotlin/com/whelksoft/flutter_native_timezone/FlutterNativeTimezonePlugin.kt
@@ -0,0 +1,49 @@
+package com.whelksoft.flutter_native_timezone
+
+import android.os.Build
+import androidx.annotation.NonNull
+import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+import io.flutter.plugin.common.MethodChannel.Result
+import java.time.ZoneId
+import java.util.*
+import kotlin.collections.ArrayList
+
+class FlutterNativeTimezonePlugin : FlutterPlugin, MethodCallHandler {
+    private lateinit var channel: MethodChannel
+
+    override fun onAttachedToEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        channel = MethodChannel(binding.binaryMessenger, "flutter_native_timezone")
+        channel.setMethodCallHandler(this)
+    }
+
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
+    }
+
+    override fun onMethodCall(call: MethodCall, result: Result) {
+        when (call.method) {
+            "getLocalTimezone" -> result.success(getLocalTimezone())
+            "getAvailableTimezones" -> result.success(getAvailableTimezones())
+            else -> result.notImplemented()
+        }
+    }
+
+    private fun getLocalTimezone(): String {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            ZoneId.systemDefault().id
+        } else {
+            TimeZone.getDefault().id
+        }
+    }
+
+    private fun getAvailableTimezones(): List<String> {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            ZoneId.getAvailableZoneIds().toCollection(ArrayList())
+        } else {
+            TimeZone.getAvailableIDs().toCollection(ArrayList())
+        }
+    }
+}
